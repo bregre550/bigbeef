f69b1afcf5aae53c4a983d173ca826b7a43ff93a
kingcoitus <167474023+kingcoitus@users.noreply.github.com>
2026-04-19 16:29:44 -0400

    updates to player scene
    i updated the player scene to be a characterBody2d and i added a collision shape and sprite2d. i also updated the player script and attached it to the player.

--- big-beef/Scripts/player.gd
+++ big-beef/Scripts/player.gd
@@ -1,1 +1,4 @@
-extends Node
+class_name Player extends CharacterBody2D
+
+var cardinal_direction : Vector2 = Vector2.DOWN
+const DIR_4 : Array[Vector2] = [Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP]
