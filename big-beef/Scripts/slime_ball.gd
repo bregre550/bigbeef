f6f49a1588d04fbb2c56a378d634757dfa212d99
kingcoitus <167474023+kingcoitus@users.noreply.github.com>
2026-05-09 19:51:35 -0400

    bug fix
    i think the stun is fixed. also made it so bullet disappear when you are shot

--- big-beef/Scripts/slime_ball.gd
+++ big-beef/Scripts/slime_ball.gd
@@ -1,9 +1,14 @@
-class_name Bullet extends CharacterBody2D
+class_name SlimeBall extends CharacterBody2D
 
-@export var speed : int = 400
+@onready var hurt_box: HurtBox = $Sprite2D/HurtBox
+
+@export var speed : int = 250
 
 var direction : Vector2 = Vector2.ZERO
 var lifetime : float = 3.0	
+
+func _ready() -> void:
+	hurt_box.area_entered.connect(_on_area_entered)
 
 func shoot(new_direction : Vector2) -> void:
 	direction = new_direction
@@ -14,5 +19,8 @@ func _physics_process(delta: float) -> void:
 	lifetime -= delta
 	if lifetime <= 0:
 		queue_free()
+		
+func _on_area_entered(_b) -> void:
+	queue_free()
 	
 	
