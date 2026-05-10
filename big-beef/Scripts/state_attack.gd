644aa3cbdf9932fc854bd41e4c80f9f9a8406067
kingcoitus <167474023+kingcoitus@users.noreply.github.com>
2026-05-10 15:08:37 -0400

    attack anim added
    its not working wahhh

---
+++ big-beef/Scripts/state_attack.gd
@@ -0,0 +1,43 @@
+class_name State_Attack extends State
+
+var attacking: bool = false
+
+@export_range(1, 20, 0.5) var decelerate_speed: float = 5.0
+
+@onready var animation: AnimationPlayer = $AnimationPlayer
+
+@onready var idle: State_Idle = $"../Idle"
+@onready var walk: State = $"../Walk"
+
+#@onready var hurt_box: HurtBox = %HurtBox
+@onready var hurt_box: HurtBox = $HurtBox
+
+func Enter() -> void:
+	player.update_animation("attack")
+	animation.play("attack_" + player.anim_direction())
+	animation.animation_finished.connect(_end_attack)
+	
+	attacking = true
+	
+func Exit() -> void:
+	animation.animation_finished.disconnect(_end_attack)
+	attacking = false
+	
+func Process(_delta: float) -> State:
+	player.velocity -= player.velocity * decelerate_speed * _delta
+	
+	if not attacking:
+		if player.direction == Vector2.ZERO:
+			return idle
+		else:
+			return walk
+	return null
+	
+func Physics( _delta: float ) -> State:
+	return null
+	
+func HandleInput(_event: InputEvent) -> State:
+	return null
+	
+func _end_attack(_new_anim_name: String) -> void:
+	attacking = false
--- big-beef/Scripts/state_idle.gd
+++ big-beef/Scripts/state_idle.gd
@@ -1,6 +1,7 @@
 class_name State_Idle extends State
 
 @onready var walk: State = $"../Walk"
+@onready var attack: State_Attack = $"../Attack"
 
 func Enter() -> void:
 	player.update_animation( "idle" )
@@ -18,7 +19,7 @@ func Physics( _delta: float ) -> State:
 	return null
 	
 func HandleInput(_event: InputEvent) -> State:
-	#if _event.is_action_pressed( "attack" ):
-		#return attack
+	if _event.is_action_pressed("attack"):
+		return attack
 		
 	return null
